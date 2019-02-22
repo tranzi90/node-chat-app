7fba745a0c79b47a3941dda089807831c8930932
tranzi90 <tranzi90@gmail.com>
2019-02-22 14:42:50 +0300

    Add io.emit on server side

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('connected to server');
-
-    socket.emit('createMessage', {
-        from: 'OmTV',
-        text: 'ptn hlo vnature',
-    });
 });
 
 socket.on('disconnect', function() { console.log('disconnected from server'); });
