e9f911abe1789122b4e685f36970c968571616ba
tranzi90 <tranzi90@gmail.com>
2019-03-16 16:48:27 +0300

    Add join / index page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Chat app' });
+  res.render('index', { title: 'Join | Chat app' });
+});
+
+router.get('/chat', function(req, res, next) {
+  res.render('chat', { title: 'Chat | Chat app' });
 });
 
 module.exports = router;
