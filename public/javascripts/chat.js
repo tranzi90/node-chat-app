936bd9809c7fb557902440e8e547aec77b1c7e23
tranzi90 <tranzi90@gmail.com>
2019-03-26 13:47:52 +0300

    Add messages to room only

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -68,7 +68,6 @@ $('#message-form').on('submit', function (e) {
     let messageTextbox = $('[name=message]');
 
     socket.emit('createMessage', {
-        from: 'User',
         text: messageTextbox.val()
     }, function () {
         messageTextbox.val('')
