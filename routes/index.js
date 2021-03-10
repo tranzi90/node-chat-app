04db75bba070a85591f02ad5badc271c3329e902
denis <tranzi90@gmail.com>
2021-03-10 23:34:27 +0300

    refactor routes

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,28 @@
-var express = require('express');
-var router = express.Router();
-const {isRealString} = require('./../bin/utils/validation');
+var express = require('express')
+var router = express.Router()
+const { isRealString } = require('./../bin/utils/validation')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'Join | Chat app',
-    bodyClass: 'class="centered-form"'
-  });
-});
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: 'Join | Chat app',
+        bodyClass: 'class="centered-form"',
+    })
+})
 
-router.get('/chat', function(req, res, next) {
-  if (!isRealString(req.query.name) || (!isRealString(req.query.room) && !isRealString(req.query.newRoom))) {
-    res.render('error', {message: 'Something went wrong...'});
-  } else {
-    res.render('chat', {
-      title: 'Chat | Chat app',
-      room: req.query.room || req.query.newRoom,
-      bodyClass: 'class="chat"'
-    });
-  }
-});
+router.get('/chat', function (req, res, next) {
+    if (
+        !isRealString(req.query.name) ||
+        (!isRealString(req.query.room) && !isRealString(req.query.newRoom))
+    ) {
+        res.render('error', { message: 'Something went wrong...' })
+    } else {
+        res.render('chat', {
+            title: 'Chat | Chat app',
+            room: req.query.room || req.query.newRoom,
+            bodyClass: 'class="chat"',
+        })
+    }
+})
 
-module.exports = router;
+module.exports = router
